7aab01f9094ba27897f3ccad1f70746cb45198b6
mikemtzp <mikemp8226@gmail.com>
2022-05-23 17:33:59 -0500

    Add complete add button functionality

--- main.js
+++ main.js
@@ -1,38 +1,35 @@
-const bookList = [
-  {
-    title: 'The lord of the rings',
-    author: 'Tolkien'
-  }
-];
+const bookList = [];
 
-const body = document.querySelector('body');
-const form = document.getElementById('form');
-const newBook = document.createElement('div');
-const title = document.createElement('p');
-const author = document.createElement('p');
-const remove = document.createElement('button');
-const line = document.createElement('hr');
-title.textContent = bookList[0].title;
-author.textContent = bookList[0].author;
-remove.textContent = 'Remove'
+const add = document.getElementById("add");
 
-newBook.append(title);
-newBook.append(author);
-newBook.append(remove);
-newBook.append(line);
-body.insertBefore(newBook, form);
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+}
 
-const add = document.getElementById('add');
-add.addEventListener('click', (e)=> {
-    e.preventDefault();
-    let newTitle = document.getElementById('title').textContent;
-    let newAuthor = document.getElementById('author').textContent;
-    let newBooklist = {
-      title : newTitle,
-      author : newAuthor
-    }
-    console.log(newBooklist)
-})
+add.addEventListener("click", (e) => {
+  e.preventDefault();
+  const newTitle = document.getElementById("title");
+  const newAuthor = document.getElementById("author");
+  let theTitle = newTitle.value;
+  let theAuthor = newAuthor.value;
+  let myBook = new Book(theTitle, theAuthor);
+  bookList.push(myBook);
+  console.log(bookList);
 
+  const body = document.querySelector('body');
+  const newBook = document.createElement("div");
+  const titlePara = document.createElement("p");
+  const authorPara = document.createElement("p");
+  const remove = document.createElement("button");
+  const line = document.createElement("hr");
+  titlePara.textContent = myBook.title;
+  authorPara.textContent = myBook.author;
+  remove.textContent = "Remove";
 
-
+  newBook.append(titlePara);
+  newBook.append(authorPara);
+  newBook.append(remove);
+  newBook.append(line);
+  body.insertBefore(newBook, form);
+});
