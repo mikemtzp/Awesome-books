4b6ed9e75b28013111645ef61e7d932145811b9a
mikemtzp <mikemp8226@gmail.com>
2022-05-25 17:11:07 -0500

    Add refactoring to all functions

--- main.js
+++ main.js
@@ -1,25 +1,20 @@
-function initiateBooksData() {
-  const initialBooks = localStorage.getItem('bookList');
-  if (initialBooks) {
-    return JSON.parse(initialBooks);
+class Storage {
+  static storeData() {
+    localStorage.setItem("bookList", JSON.stringify(bookList));
   }
-  return [];
-}
 
-let bookList = initiateBooksData();
-
-function generateId() {
-  if (bookList.length > 0) {
-    return bookList[bookList.length - 1].id + 1;
+  static initiateBooksData() {
+    const initialBooks = localStorage.getItem("bookList");
+    if (initialBooks) {
+      return JSON.parse(initialBooks);
+    }
+    return [];
   }
-  return -1;
 }
 
-function storeData() {
-  localStorage.setItem('bookList', JSON.stringify(bookList));
-}
+let bookList = Storage.initiateBooksData();
 
-const add = document.getElementById('add');
+const add = document.getElementById("add");
 
 class Books {
   constructor(title, author, id) {
@@ -28,58 +23,67 @@ class Books {
     this.id = id;
   }
 
-  createBook({ title, author, id }) {
-    const newBook = document.createElement('div');
-    const titlePara = document.createElement('p');
-    const authorPara = document.createElement('p');
-    const remove = document.createElement('button');
-    remove.setAttribute('id', this.id);
+   static createBook({ title, author, id }) {
+    const newBook = document.createElement("div");
+    const titlePara = document.createElement("p");
+    const authorPara = document.createElement("p");
+    const remove = document.createElement("button");
+    remove.setAttribute("id", this.id);
     titlePara.textContent = this.title;
     authorPara.textContent = this.author;
     const completeBook = `"${title}" by ${author}`;
-    remove.textContent = 'Remove';
-    newBook.classList.toggle('grey', id % 2 !== 0);
-    newBook.classList.toggle('white', id % 2 === 0);
-    newBook.classList.add('position');
-    remove.classList.add('remove');
+    remove.textContent = "Remove";
+    newBook.classList.toggle("grey", id % 2 !== 0);
+    newBook.classList.toggle("white", id % 2 === 0);
+    newBook.classList.add("position");
+    remove.classList.add("remove");
 
     newBook.append(completeBook);
     newBook.append(remove);
 
-    remove.addEventListener('click', (e) => {
+    remove.addEventListener("click", (e) => {
       e.preventDefault();
       newBook.remove();
       bookList = bookList.filter((book) => book.id !== id);
-      storeData();
+      Storage.storeData();
     });
     return newBook;
   }
+
+  static generateId() {
+    if (bookList.length > 0) {
+      return bookList[bookList.length - 1].id + 1;
+    }
+    return -1;
+  }
 }
 
-function renderBooks() {
-  const bookSection = document.querySelector('.box');
-  const bookListElement = document.createElement('div');
-  bookListElement.id = 'bookList';
-  bookList.forEach((book) => {
-    const bookElem = new Books(book.title, book.author, book.id);
-    const bookElement = bookElem.createBook(bookElem);
-    bookSection.append(bookElement);
-  });
+class Render {
+  static renderBooks() {
+    const bookSection = document.querySelector(".box");
+    const bookListElement = document.createElement("div");
+    bookListElement.id = "bookList";
+    bookList.forEach((book) => {
+      const bookElem = new Books(book.title, book.author, book.id);
+      const bookElement = bookElem.createBook(bookElem);
+      bookSection.append(bookElement);
+    });
+  }
 }
 
-add.addEventListener('click', (e) => {
+add.addEventListener("click", (e) => {
   e.preventDefault();
-  const newTitle = document.getElementById('title');
-  const newAuthor = document.getElementById('author');
+  const newTitle = document.getElementById("title");
+  const newAuthor = document.getElementById("author");
   const theTitle = newTitle.value;
   const theAuthor = newAuthor.value;
-  const theId = generateId();
+  const theId = Books.generateId();
   const myBook = new Books(theTitle, theAuthor, theId);
-  const bookElement = myBook.createBook(myBook);
-  const bookListElement = document.querySelector('.box');
+  const bookElement = Books.createBook(myBook);
+  const bookListElement = document.querySelector(".box");
   bookListElement.append(bookElement);
   bookList.push(myBook);
-  storeData();
+  Storage.storeData();
 });
 
-renderBooks();
+Render.renderBooks();
