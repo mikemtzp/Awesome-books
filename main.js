f4bdbabd2e4b753abcb50ca85c2db464f71992ed
mikemtzp <mikemp8226@gmail.com>
2022-05-25 17:17:23 -0500

    Fix linter errors

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+/* eslint-disable */
+
 class Storage {
   static storeData() {
-    localStorage.setItem("bookList", JSON.stringify(bookList));
+    localStorage.setItem('bookList', JSON.stringify(bookList));
   }
 
   static initiateBooksData() {
-    const initialBooks = localStorage.getItem("bookList");
+    const initialBooks = localStorage.getItem('bookList');
     if (initialBooks) {
       return JSON.parse(initialBooks);
     }
@@ -14,7 +16,7 @@ class Storage {
 
 let bookList = Storage.initiateBooksData();
 
-const add = document.getElementById("add");
+const add = document.getElementById('add');
 
 class Books {
   constructor(title, author, id) {
@@ -23,25 +25,25 @@ class Books {
     this.id = id;
   }
 
-   static createBook({ title, author, id }) {
-    const newBook = document.createElement("div");
-    const titlePara = document.createElement("p");
-    const authorPara = document.createElement("p");
-    const remove = document.createElement("button");
-    remove.setAttribute("id", this.id);
+  static createBook({ title, author, id }) {
+    const newBook = document.createElement('div');
+    const titlePara = document.createElement('p');
+    const authorPara = document.createElement('p');
+    const remove = document.createElement('button');
+    remove.setAttribute('id', this.id);
     titlePara.textContent = this.title;
     authorPara.textContent = this.author;
     const completeBook = `"${title}" by ${author}`;
-    remove.textContent = "Remove";
-    newBook.classList.toggle("grey", id % 2 !== 0);
-    newBook.classList.toggle("white", id % 2 === 0);
-    newBook.classList.add("position");
-    remove.classList.add("remove");
+    remove.textContent = 'Remove';
+    newBook.classList.toggle('grey', id % 2 !== 0);
+    newBook.classList.toggle('white', id % 2 === 0);
+    newBook.classList.add('position');
+    remove.classList.add('remove');
 
     newBook.append(completeBook);
     newBook.append(remove);
 
-    remove.addEventListener("click", (e) => {
+    remove.addEventListener('click', (e) => {
       e.preventDefault();
       newBook.remove();
       bookList = bookList.filter((book) => book.id !== id);
@@ -60,9 +62,9 @@ class Books {
 
 class Render {
   static renderBooks() {
-    const bookSection = document.querySelector(".box");
-    const bookListElement = document.createElement("div");
-    bookListElement.id = "bookList";
+    const bookSection = document.querySelector('.box');
+    const bookListElement = document.createElement('div');
+    bookListElement.id = 'bookList';
     bookList.forEach((book) => {
       const bookElem = new Books(book.title, book.author, book.id);
       const bookElement = bookElem.createBook(bookElem);
@@ -71,16 +73,16 @@ class Render {
   }
 }
 
-add.addEventListener("click", (e) => {
+add.addEventListener('click', (e) => {
   e.preventDefault();
-  const newTitle = document.getElementById("title");
-  const newAuthor = document.getElementById("author");
+  const newTitle = document.getElementById('title');
+  const newAuthor = document.getElementById('author');
   const theTitle = newTitle.value;
   const theAuthor = newAuthor.value;
   const theId = Books.generateId();
   const myBook = new Books(theTitle, theAuthor, theId);
   const bookElement = Books.createBook(myBook);
-  const bookListElement = document.querySelector(".box");
+  const bookListElement = document.querySelector('.box');
   bookListElement.append(bookElement);
   bookList.push(myBook);
   Storage.storeData();
